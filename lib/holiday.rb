f6cfedcccc9f4cb633e7dce9802a22df8dd98a7a
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-16 14:00:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, events|
     puts "#{season.to_s.capitalize!}:"
     events.each do |name, supplies|
-      formatted_name = name.to_s.split('_')
+      string_name = name.to_s.split('_')
       formatted_name.map { |e| e.capitalize!  }
       puts formatted_name
     end
