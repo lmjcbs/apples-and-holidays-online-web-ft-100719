7af1184168042eba56c2685c730ec190f1b6ea4f
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-16 14:13:00 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,39 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter].each do |event, values|
+    values << supply
+  end
+  holiday_hash
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  winter_supplies = Array.new
+  holiday_hash[:winter].each do |event, supplies|
+    supplies.each { |item| winter_supplies << item }
+  end
+  winter_supplies
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,18 +61,25 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, events|
+    puts "#{season.to_s.capitalize!}:"
+    events.each do |name, supplies|
+      array_name = name.to_s.split('_')
+      formatted_name = array_name.map { |e| e.capitalize!  }
+      formatted_supplies = supplies.join(', ')
+      puts "  #{formatted_name.join(' ')}: #{formatted_supplies}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holidays_with_bbq = Array.new
+  holiday_hash.each do |season, event|
+    holiday_hash[season].each do |event, supplies|
+      supplies.each { |e| holidays_with_bbq << event if e == "BBQ"}
+    end
+  end
+  holidays_with_bbq
 end
-
-
-
-
-
-
-
